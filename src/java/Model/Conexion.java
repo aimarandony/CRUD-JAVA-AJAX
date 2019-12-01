39f8d6b28df9e60517f3d2b9baa02ecddec882e0
Aimar Andony <aimarandony@gmail.com>
2019-12-01 00:51:27 -0500

    "second commit - Conexion.java, index.html"

--- src/java/Model/Conexion.java
+++ src/java/Model/Conexion.java
@@ -9,7 +9,7 @@ public class Conexion {
     public Connection conectar(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            conectar = DriverManager.getConnection("jdbc:mysql://localhost:3306/BDCrudJavaAjax","root","");
+            conectar = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_example_java_crud","root","");
         }catch (ClassNotFoundException | SQLException e){
             System.out.println("Error al conectar: "+e.getMessage());
         }
